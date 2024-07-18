0d03b0fb58c4ce3ab16e01078851513b9820e566
mk26710 <type-iv@mk26710.dev>
2024-07-18 16:10:53 +0400

    fix: eslint ignore is a bad idea, rules adjusted

--- backend/src/zod-validation/zod-validation.pipe.ts
+++ backend/src/zod-validation/zod-validation.pipe.ts
@@ -4,8 +4,7 @@ import { ZodSchema } from "zod";
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  transform(value: unknown, metadata: ArgumentMetadata) {
+  transform(value: unknown, _: ArgumentMetadata) {
     try {
       const parsedValue = this.schema.parse(value);
       return parsedValue;
